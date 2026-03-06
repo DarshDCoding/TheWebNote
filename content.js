502b7fdb3b8f8a1c50cbd33a55580652ec1cac86
Darsh D. Coding <darshdcoding@gmail.com>
2026-03-07 01:46:04 +0530

    1. adding trasition to preview image, 2. adding styles for nothing to show svg, 3. redering data from content.js

--- content.js
+++ content.js
@@ -1,130 +1,95 @@
+import { activeTabUrl, getActiveTabUrl, urlExtract } from "./utils/urls.js";
+import { getCurrentDateTime } from "./utils/date.js";
+import { dummyData } from "./utils/dummyData.js";
 //elements
 const taskInputField = document.getElementById("taskInputField");
-const addTaskbtn = document.getElementById ("addTask");
-const darshboardBtn = document.getElementById ("dashboardBtn");
+const addTaskbtn = document.getElementById("addTask");
 const inputImage = document.getElementById("inputImage");
 const previewImage = document.getElementById("previewImage");
 const previewImgContainer = document.getElementById("previewImgContainer");
-const imageInputIcon = document.getElementById("imageInputIcon");
-
-//getting current date
-function getCurrentDateTime() {
-  const now = new Date();
-
-  const year = now.getFullYear();
-  const month = String(now.getMonth() + 1).padStart(2, "0");
-  const day = String(now.getDate()).padStart(2, "0");
-
-  let hours = now.getHours();
-  const minutes = String(now.getMinutes()).padStart(2, "0");
-  const seconds = String(now.getSeconds()).padStart(2, "0");
-
-  const ampm = hours >= 12 ? "PM" : "AM";
-
-  hours = hours % 12;
-  hours = hours ? hours : 12; // 0 becomes 12
-  hours = String(hours).padStart(2, "0");
-
-  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds} ${ampm}`;
-}
-
-// to get the url of active tab (hostname only)
-let activeTabUrl = "";
-
-const getActiveTabUrl = (callback) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const activeTab = tabs[0];
-    const activeTabUrl = activeTab.url;
-    callback(activeTabUrl);
-  });
-};
-
-getActiveTabUrl((url) => {
-  activeTabUrl = url;
-});
-
-const urlExtract = (url) => {
-  const urlArray = url.split("/");
-  return urlArray[2];
-};
-
 
 //taking input
-const InputProcessing = () =>{
-    let trimmedValue;
-    let noteValue;
-    let notePriority;
+const InputProcessing = () => {
+  let trimmedValue;
+  let noteValue;
+  let notePriority;
 
-    if (taskInputField.value){
-        trimmedValue = String(taskInputField.value).trim()
-        noteValue = trimmedValue.slice(0,-4)
-        notePriority = trimmedValue.slice(-4, )
-        taskInputField.value =""
-    }
+  if (taskInputField.value) {
+    trimmedValue = String(taskInputField.value).trim();
+    noteValue = trimmedValue.slice(0, -4);
+    notePriority = trimmedValue.slice(-4);
+    taskInputField.value = "";
+  }
 
-    let formatedNote = {note:noteValue, createdAt: getCurrentDateTime(), status:"pending"}
-    
-    if (notePriority === "#imp"){
-        formatedNote.priority = "important"
-    }else if (notePriority === "#neu"){
-        formatedNote.priority = "neutral"
-    }else{
-        formatedNote.note = trimmedValue
-        formatedNote.priority = "normal"
-    }
+  let formatedNote = {
+    note: noteValue,
+    createdAt: getCurrentDateTime(),
+    status: "pending",
+  };
 
-    return formatedNote
-}
+  if (notePriority === "#imp") {
+    formatedNote.priority = "important";
+  } else if (notePriority === "#neu") {
+    formatedNote.priority = "medium";
+  } else {
+    formatedNote.note = trimmedValue;
+    formatedNote.priority = "normal";
+  }
+
+  return formatedNote;
+};
 
 //preview the Image
-inputImage.addEventListener('change',()=>{
-    const file = inputImage.files[0];
+inputImage.addEventListener("change", (event) => {
+  const file = event.target.files[0];
 
-    if (file) {
-        const imageUrl = URL.createObjectURL(file);
-        previewImage.src = imageUrl;
-        previewImgContainer.style.setProperty('--preview-bg', `url(${imageUrl})`);
-        previewImgContainer.style.display= "flex";
-    }
+  console.log(file)
+
+
+  if (file) {
+    const imageUrl = URL.createObjectURL(file);
+    previewImage.src = imageUrl;
+    previewImgContainer.style.setProperty("--preview-bg", `url(${imageUrl})`);
+    previewImgContainer.style.height = "300px";
+  }
 });
 
 //Submit for keyboard Enter
-taskInputField.addEventListener("keypress", e => {
-    if (e.key == "Enter" && !e.shiftKey){
-        e.preventDefault()
-        console.log(InputProcessing())
-    }
+taskInputField.addEventListener("keypress", (e) => {
+  if (e.key == "Enter" && !e.shiftKey) {
+    e.preventDefault();
+    console.log(InputProcessing());
+  }
 });
 
 //Submit for mouse
-addTaskbtn.addEventListener("click", ()=>{
-    InputProcessing()
+addTaskbtn.addEventListener("click", () => {
+  InputProcessing();
+  previewImage.src = "";
+  previewImgContainer.style.height = "0px";
+});
+
+const renderElement = (note) => `
+    <div class="task-card priority-${note.priority}">
+
+        ${note.img ? `<img src="${note.img}" alt="Task Image" class="task-image">` : ""}
+        <div class="task-content">
+            <p class="task-desc">${note.note}</p>
+            <div class="task-footer">
+                <p class="btn-action date">${note.createdAt}</p>
+                <button class="btn-action btn-edit" data-id="${note.id}">Edit</button>
+                <button class="btn-action btn-delete" data-id="${note.id}">Delete</button>
+            </div>
+        </div>
+    </div>
+`;
+
+let renderNotes = "";
+
+if (dummyData) {
+  renderNotes += dummyData.important.map(note => renderElement(note)).join("");
+  renderNotes += dummyData.medium.map(note =>renderElement(note)).join("");
+  renderNotes += dummyData.normal.map(note => renderElement(note)).join("");
 }
-)
 
-
-
-// opeining database
-let db;
-
-const request = indexedDB.open("TheWebNoteDB", 1)
-
-request.onupgradeneeded = event =>{
-    db = event.target.result;
-
-  if (!db.objectStoreNames.contains("notes")) {
-    const store = db.createObjectStore("notes", { keyPath: "url" });
-  }
-    console.log ("DB Setup complete.")
-}
-
-request.onsuccess = event =>{
-    db = event.target.result;
-    console.log ("Database Created...");
-}
-
-request.onerror = e => console.error(`Database Error: ${e.target.error?.message}`)
-
-
-
-
+document.getElementById("task-container").innerHTML = renderNotes;
